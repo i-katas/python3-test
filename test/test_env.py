a666ad9db5161e09748df77451de68593ecdcee9
i-katas <feexonj@gmail.com>
2020-04-01 20:34:28 +0800

    test: load python configuration

--- test/test_env.py
+++ test/test_env.py
@@ -1,2 +1,12 @@
-def test_env():
-    pass
+import configparser as cp
+from os import path
+
+config_file = path.join(path.dirname(__file__), '../setup.cfg')
+
+
+def test_config():
+    config = cp.ConfigParser()
+
+    assert config.read(config_file, encoding='utf8') == [config_file]
+    assert 'metadata' in config, 'section [metadata] is not exists'
+    assert config['metadata']['name'] == 'python3-test'
