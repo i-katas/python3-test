9c068e0b9be5eee2d1b0c234af7bcaedf5a45d98
i-katas <feexonj@gmail.com>
2020-04-04 00:47:16 +0800

    test: duck type checking

---
+++ test/test_duck_typing.py
@@ -0,0 +1,19 @@
+from typing import runtime_checkable, Protocol, TypeVar, abstractmethod, Generic
+
+T = TypeVar('T')
+
+
+@runtime_checkable
+class Consumer(Protocol[T]):
+    __slots__ = ()
+
+    @abstractmethod
+    def __accept__(self, value: T): pass
+
+
+def test_duck_type_checking():
+    class MessageReceiver(Generic[T]):
+        def __accept__(self, value: T):
+            pass
+
+    assert isinstance(MessageReceiver(), Consumer)
