75f5a8cb780a01c016f13e48b0f902d044bb6dc8
i-katas <feexonj@gmail.com>
2020-04-03 09:44:58 +0800

    test: function flags

--- test/test_function.py
+++ test/test_function.py
@@ -19,3 +19,26 @@ def test_transformation_from_function_object_to_instance_method_object_happens_e
     assert methods.test.__self__ is methods
     assert methods.test is not methods.test
     assert methods.after is methods.after
+
+
+def empty(): pass
+def one(_): pass
+def kwargs(**_): pass
+def args(*_): pass
+def generator(): yield 
+async def asynchronous(): pass
+function = type(empty)
+
+def test_arguments_flags():
+    def flags_of(fun: function): return fun.__code__.co_flags
+    def assert_flags_of(func, flags, message):
+        assert (diff := flags_of(empty) ^ flags_of(func)) == flags_of(func) & flags == flags, (flags_of(empty), flags_of(func), flags, message)
+
+    assert type(flags_of) is function
+    assert_flags_of(args, 0x04, 'arbitray args flags') 
+    assert_flags_of(kwargs, 0x08, 'keyword arguments flags') 
+    assert_flags_of(generator, 0x20, 'generator function flags') 
+    assert_flags_of(asynchronous, 0x80, 'arbitray args flags') 
+    assert_flags_of(flags_of, 0x10, 'nested scope function flags') 
+    
+
